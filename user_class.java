97cec455b1a0dd63a3c2fdb0958eec062ff68246
HP <HP@DESKTOP-V3AUBOB>
2019-12-11 20:36:00 +0600

    Signed-off-by:HP <HP@DESKTOP-V3AUBOB>

---
+++ user_class.java
@@ -0,0 +1,112 @@
+public class user_class {
+    private String name;
+    private String age;
+    private String gmail;
+    private String phone;
+    private String address;
+    private String password;
+    public String m1,m2,m3,m4,m5;
+
+    public user_class(String name, String age, String gmail, String phone, String address, String password, String m1, String m2, String m3, String m4, String m5) {
+        this.name = name;
+        this.age = age;
+        this.gmail = gmail;
+        this.phone = phone;
+        this.address = address;
+        this.password = password;
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+        this.m4 = m4;
+        this.m5 = m5;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getM1() {
+        return m1;
+    }
+
+    public String getM2() {
+        return m2;
+    }
+
+    public String getM3() {
+        return m3;
+    }
+
+    public String getM4() {
+        return m4;
+    }
+
+    public String getM5() {
+        return m5;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setM1(String m1) {
+        this.m1 = m1;
+    }
+
+    public void setM2(String m2) {
+        this.m2 = m2;
+    }
+
+    public void setM3(String m3) {
+        this.m3 = m3;
+    }
+
+    public void setM4(String m4) {
+        this.m4 = m4;
+    }
+
+    public void setM5(String m5) {
+        this.m5 = m5;
+    }
+    
+}
